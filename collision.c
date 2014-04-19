f1468fe3293634036dcbdfb6046870194815dad4
Ryan Dougherty <ryand626@gmail.com>
2014-04-19 19:15:33 -0400

    Fixed collision
    collision now works and more collision states can be added with custom
    timeouts

--- collision.c
+++ collision.c
@@ -1,7 +1,7 @@
 #include <TimerOne.h>
-
+ 
 // Motor constants
-
+ 
 // THESE ONES ARE PINS
 #define MTRLFWD 6 
 #define MTRLREV 7
@@ -9,7 +9,7 @@
 #define MTRRREV 9
 #define MTRLEN 14
 #define MTRREN 15
-
+ 
 // JUST CONSTANTS
 #define STOPPED 0
 #define FORWARD 1
@@ -20,28 +20,11 @@
 #define RSWINGR 6 // reverse swing right
 #define RSWINGL 7
 #define REVERSE 8
-
+ 
 #define SLOW 60 
 #define MEDIUM 125
 #define FAST 255
-
-// Color Sensor Constants
-//Pins
-//#define REDLED 6
-//#define BLUELED 7
-
-#define LIGHTSENSOR A0
-
-//States
-#define BLUESTATE 0
-#define REDSTATE 1
-
-#define DARK_THRESHOLD 200
-
-#define DARK 0
-#define RED 1
-#define BLUE 2
-
+ 
 // State Constants
 #define COUNTER_LENGTH 10
 #define MAX_RUNTIME 5000
@@ -50,117 +33,86 @@
 #define RIGHT 2
 #define LEFT 3
 #define HALT 6
-
+ 
 // Collision Pads
 #define PAD1 22
 #define PAD2 23
 #define PAD3 24
 #define PAD4 25
-
-
-
-// Color variables
-int colorState;
-int currentColorMeasurement, lastColorMeasurement;
-int blueValue, redValue;
-int lastColorReturned;
-
+ 
+#define PAD1_STATE2 10
+#define PAD2_STATE2 11
+#define PAD3_STATE2 12
+#define PAD4_STATE2 13
+ 
 // State variables
 int counter;
 int state;
-
+ 
 // Motor Functions
 void setMotors(int,int);
-
+ 
 // Color Sensor Functions
 int colorValue();
 void colorMeasure();
-
+ 
 // State functions
 void move(int state);
 int checkstate(int state);
-
+ 
 // Collision variables
 int previousState;
 int collisionTimer;
 int collisonTimeout;
 bool collide;
-
-int collisionState;
-
-
+ 
 void setup() {
- //motor setup
- pinMode(MTRLFWD,OUTPUT);
- pinMode(MTRRFWD,OUTPUT);
- pinMode(MTRLREV,OUTPUT);
- pinMode(MTRRREV,OUTPUT);
- pinMode(MTRLEN,OUTPUT);
- pinMode(MTRREN,OUTPUT);
- digitalWrite(MTRLEN, LOW);
- digitalWrite(MTRREN, LOW);
- collide = false;
-
- //Color Sensor Setup
- //pinMode(REDLED,OUTPUT);
- //pinMode(BLUELED,OUTPUT);
+  //motor setup
+  pinMode(MTRLFWD,OUTPUT);
+  pinMode(MTRRFWD,OUTPUT);
+  pinMode(MTRLREV,OUTPUT);
+  pinMode(MTRRREV,OUTPUT);
+  pinMode(MTRLEN,OUTPUT);
+  pinMode(MTRREN,OUTPUT);
+  digitalWrite(MTRLEN, LOW);
+  digitalWrite(MTRREN, LOW);
  
- //digitalWrite(REDLED, HIGH);
- //digitalWrite(BLUELED,LOW);
+  // Initial motor state
+  state = HALT;
  
- //colorState = REDSTATE;
- 
- //lastColorMeasurement = DARK;
- //lastColorReturned = DARK;
- 
- //Interrupt Setup
- //Timer1.initialize(15000); //25 milliseconds
- //Timer1.attachInterrupt(colorMeasure);
- state = FORWARD;
-
- // Collision setup
- attachInterrupt(1,collision, RISING);
- collisionTimer = 0;
- collisonTimeout = 0;
+  // Collision setup
+  collisionTimer = 0;
+  collisonTimeout = 0;
+  collide = false;
+  attachInterrupt(1,collision, RISING);
 }
-
+ 
 void loop() {
- // While not in the halting state, update the state with the sensors
- // then move according to state
- //int color = colorValue();
-  if (millis() > MAX_RUNTIME){
-    state = HALT;
-  }
-  if(collide){
+  //don't let it run forever
+
+  // If colliding, reposition
+  if(collide == true){
     reposition();
+  // If not colliding, move according to state
+  }else{
+    switch (state) {
+      case FORWARD:
+        setMotors(FORWARD,SLOW);
+        break;
+      case RIGHT:
+        setMotors(FSWINGR,SLOW);
+        break;
+      case LEFT:
+        setMotors(FSWINGL,SLOW);
+        break;
+      default:
+        setMotors(STOPPED,SLOW);
+        break;
+    } 
   }
-
- switch (state) {
-   case FORWARD:
-     setMotors(FORWARD,SLOW);
-     // if (color == BLUE) {
-     //   state = RIGHT;
-     // }
-     break;
-   case RIGHT:
-     setMotors(FSWINGR,SLOW);
-     // if (color == DARK) {
-     //   state = LEFT;
-     // }
-     break;
-   case LEFT:
-     setMotors(FSWINGL,SLOW);
-     // if (color == BLUE) {
-     //   state = RIGHT;
-     // }
-     break;
-   default:
-     setMotors(STOPPED,SLOW);
-     break;
- } 
 }
-
-
+ 
+ 
 // Set direction, velocity
 void setMotors(int i, int v) {
  switch(i) {
@@ -236,69 +188,107 @@ void setMotors(int i, int v) {
      digitalWrite(MTRLEN,LOW);
      digitalWrite(MTRLEN,LOW);
      break;
- }
- state = i;
+  }
 }
-
+ 
+// YOU DON COLLID
 void collision(){
-  previousState = state;
+  // Store previous state
+  if(!collide){
+    previousState = state;
+  }
+  // Set current state and know that you collided
   state = checkPads();
   collide = true;
+  // Set the timeout of the collision movement to 500ms from now
+  collisonTimeout = 500;
+  // Set the current time of collision
+  collisionTimer = millis();
   //communicateCollision();
 }
-
+ 
+// return the pad that got hit, starting the check at PAD1
+// Eventually will have different timeouts here
 int checkPads(){
-  for (int i = PAD1; i < PAD4; i++)
-  {
+  for (int i = PAD1; i < PAD4; i++){
     if(digitalRead(i) == HIGH){
       return i;
     }
   }
   return 0;
 }
-
-bool checkLastCollision(){
-  // checks to see if it's been enough time since the last collision
+ 
+// checks to see if it's been enough time since the last collision
+bool timeOut(){
   if(millis() > collisionTimer + collisonTimeout){
    return true;
   } else{
     return false;
   }
 }
-
+ 
+// Say where you were hit
 void communicateCollision(){
-  // Say where you were hit
 }
-
+ 
+// reposition according to pad that was hit, and the last time you were hit
+// ACTUALLY MOVE ACCORDINGLY
 void reposition(){
-  // reposition according to pad that was hit, and the last time you were hit
-  // ACTUALLY MOVE ACCORDINGLY
-  if(checkLastCollision){
     switch(state){  
       case PAD1: 
         setMotors(REVERSE,MEDIUM);
-        delay(5000);
-        setMotors(ROTATER,MEDIUM);
+        if(timeOut()){
+          collisonTimeout = 500;
+          collisionTimer = millis();
+          state = PAD1_STATE2;
+        }
         break;
+      case PAD1_STATE2:
+        setMotors(FSWINGR,MEDIUM);
+        if(timeOut()){
+          endCollide();
+        }
+        break;  
       case PAD2:
-        setMotors(REVERSE, MEDIUM);
-        delay(5000);
         setMotors(FSWINGR, MEDIUM);
+        if(timeOut()){
+          collisonTimeout = 500;
+          collisionTimer = millis();
+          state = PAD2_STATE2;
+        }
+        break;
+      case PAD2_STATE2:
+        endCollide();
         break;
       case PAD3:
-        setMotors(REVERSE, MEDIUM);
-        delay(5000);
         setMotors(ROTATEL, MEDIUM);
+        if(timeOut()){
+          collisonTimeout = 500;
+          collisionTimer = millis();
+          state = PAD3_STATE2;
+        }
+        break;
+      case PAD3_STATE2:
+        endCollide();
         break;
       case PAD4:
-        setMotors(REVERSE, MEDIUM);
-        delay(5000);
         setMotors(FSWINGR, MEDIUM);
+        if(timeOut()){
+          collisonTimeout = 500;
+          collisionTimer = millis();
+          state = PAD4_STATE2;
+        }
+        break;
+      case PAD4_STATE2:
+        endCollide();
         break;  
       default:
+        endCollide();
         break;
     }
-    collisionTimer = millis();
-  }
+}
+ 
+void endCollide(){
+  state = previousState;
   collide = false;
 }
